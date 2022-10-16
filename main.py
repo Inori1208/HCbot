001524c1a3c312ff251934e63a5f1e696414c366
Inori1208 <x65124x@gmail.com>
2022-10-16 15:32:01 +0800

    HCBot alpha 1.1

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import discord
 import os
 from discord.ext import commands
+import json
+
+with open('setting.json','r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='&',intents=intents)
-test_channel = bot.get_channel(1025052102063554580)
 
 #Start
 
@@ -24,4 +27,4 @@ async def ping(ctx):
 
 
 
-bot.run('MTAyODY4OTEyNDA5NTQzNDc1Mg.G4zOyC.chsO03Opgpfihs-B0S4O4KvqyOeuGSahQ7VTto')
+bot.run(jdata['TOKEN'])
