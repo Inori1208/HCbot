6e60adfd2beb76522825bf6e14061e41f820d642
Inori1208 <x65124x@gmail.com>
2022-10-18 00:28:21 +0800

    HCBot alpha 2.0
    Simply classify the archives, making the subsequent update easier to code and differentiate each commands or functions.

---
+++ bot.py
@@ -0,0 +1,47 @@
+import discord
+import os
+from discord.ext import commands
+import json
+import asyncio
+
+with open('setting.json','r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='&',intents=intents)
+
+#Start
+
+@bot.event
+async def on_ready():
+    print('>> {0.user} 已成功載入。 <<'.format(bot))
+
+@bot.command()
+async def load(ctx,extension):
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.send(f'{extension} is loaded.')
+
+@bot.command()
+async def unload(ctx,extension):
+    bot.unload_extension(f'cmds.{extension}')
+    await ctx.send(f'{extension} is unloaded.')
+
+@bot.command()
+async def reload(ctx,extension):
+    bot.reload_extension(f'cmds.{extension}')
+    await ctx.send(f'{extension} is reloaded.')
+
+#search file in cmds
+async def load_extensions():
+    for filename in os.listdir('./cmds'):
+        print(filename)
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cmds.{filename[:-3]}')
+
+if __name__ == "__main__":
+    async def main():
+        async with bot:
+            await load_extensions()
+            await bot.start(jdata['TOKEN'])
+
+    asyncio.run(main())
